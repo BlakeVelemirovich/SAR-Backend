9d0ffc95ae6efcfb3eb4cc607b39d21991fa8565
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2025-03-17 10:12:14 -0300

    DBContext configuration

--- SAR-API/SAR-API/Database/NeonDbContext.cs
+++ SAR-API/SAR-API/Database/NeonDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SAR_API.Domains;
 
 namespace SAR_API.Database;
 
@@ -8,5 +9,8 @@ public class NeonDbContext : DbContext
     {
     }
     
+    public DbSet<Agency> Agencies { get; set; }
+    public DbSet<Responder> Responders { get; set; }
+    public DbSet<User> Users { get; set; }
     // public DbSet<Incident> Incidents { get; set; }
 }
---
+++ SAR-API/SAR-API/Domains/Agency.cs
@@ -0,0 +1,10 @@
+namespace SAR_API.Domains;
+
+public class Agency
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Province { get; set; }
+}
