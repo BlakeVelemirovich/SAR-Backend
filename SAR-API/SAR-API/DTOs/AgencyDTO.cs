3f03a2b1f095bdcceb3dfb299123bca7003d9d86
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2025-03-25 13:29:16 -0300

    Added fix to agency province issue

--- SAR-API/SAR-API/DTOs/AgencyDTO.cs
+++ SAR-API/SAR-API/DTOs/AgencyDTO.cs
@@ -1,15 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SAR_API.DTOs;
 
 public class AgencyDTO
 {
+    // Primary Key
+    [Key]
     [Column("agency_id")]
     public Guid AgencyId { get; set; }
     
     [Column("agency_name")]
     public string AgencyName { get; set; }
     
-    [Column("Province")]
+    [Column("province")]
     public string Province { get; set; }
 }
